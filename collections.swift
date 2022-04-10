17074a2e190b0c67790583581554efa72270bdb8
MY <715711877@qq.com>
2022-04-10 14:41:21 +0800

    array

---
+++ collections.swift
@@ -0,0 +1,13 @@
+//
+//  collections.swift
+//  swift-learning
+//
+//  Created by mymac on 2022/4/10.
+//
+
+import Foundation
+
+var dict: [String: String] = ["name": "cxk", "age": "18"]
+var dict1: Dictionary<String, String> = [:]
+
+print(dict["name"])
