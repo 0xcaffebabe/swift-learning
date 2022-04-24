b91e50a19d9d4f236bd7d38d36d816b879b214eb
MY <715711877@qq.com>
2022-04-24 20:33:05 +0800

    error-handles

---
+++ error-handles.swift
@@ -0,0 +1,40 @@
+//
+//  error-handles.swift
+//  swift-learning
+//
+//  Created by mymac on 2022/4/24.
+//
+
+import Foundation
+
+enum RuntimeError: Error{
+    case NetWorkError(String)
+    case ReadTimeOutError
+}
+
+func main(a: Int) throws -> Void {
+    // 函数执行结束后才会执行
+    defer {
+        print("finally2 execute")
+    }
+    defer {
+        print("finally1 execute")
+    }
+    if a == 1 {
+        throw RuntimeError.NetWorkError("unknow host")
+    }
+}
+// 强制忽略异常 发生异常程序就会崩
+try! main(a:2)
+// 忽略异常 发生异常不会蹦
+try? main(a: 1)
+
+do {
+    try main(a: 1)
+}catch RuntimeError.NetWorkError(let e) {
+    print("error", e)
+}catch let e as RuntimeError {
+    print(e)
+}catch {
+    print("unknow error")
+}
